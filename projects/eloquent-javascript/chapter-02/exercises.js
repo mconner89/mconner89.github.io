1cd727950fc3742ef42f42c2bfd3523dbfbc4db8
Ubuntu <ubuntu@ip-172-31-38-1.ec2.internal>
2020-04-08 18:41:17 +0000

    adding matchy psuedocode to github

--- projects/eloquent-javascript/chapter-02/exercises.js
+++ projects/eloquent-javascript/chapter-02/exercises.js
@@ -3,15 +3,45 @@
 // triangles ///////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-function triangles(number) {
-
+function triangles(numbers){
+    // creates empty string variable to hold the #s
+    var tri = '';  
+    // start: 1
+    // stop: numbers
+    for(var i = 1; i <= numbers; i++) {
+      // takes tri variable and adds a # to it for every loop
+      tri += '#';
+      // logs tri variable every loop
+      console.log(tri);
+  }
 }
+
 ////////////////////////////////////////////////////////////////////////////////
 // fizzBuzz ////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
+// function named fizzBuzz that accepts two inputs
 function fizzBuzz(start, end) {
-  
+  // start: start input
+  // stop: end input
+  for(var i = start; i <= end; i++) {
+    // checks if i is divisible by 3 and 5
+    if ((i % 3 === 0) && (i % 5 === 0)) {
+      // if true, prints => fizzbuzz
+      console.log('fizzbuzz');
+    // checks if i is divisible by 3
+    } else if(i % 3 === 0) {
+      // if true, prints => fizz
+      console.log('fizz');
+    // checks if i is divisible by 5
+    } else if(i % 5 === 0) {
+      // if true, prints => buzz
+      console.log('buzz');
+    // if no other statements are true, prints => i
+    } else {
+      console.log(i);
+    }
+  } 
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -19,7 +49,25 @@ function fizzBuzz(start, end) {
 ////////////////////////////////////////////////////////////////////////////////
 
 function drawChessboard(number) {
-
+  var hashes = '';
+  // loop that prints horizontal size
+  for(var i = 1; i <= number; i++) {
+    // loop that prints vertically
+    for(var ii = 1; ii <= number; ii++) {
+      // checks if we're on an even or odd space
+      if ((ii + i) % 2 === 0) {
+        // if odd, place a ' ' in the row
+        hashes += ' ';
+      } else {
+        // if even, place a '#' in the row
+        hashes += '#';
+      }
+    }
+    // place a line break after each loop
+    hashes += '\n';
+  }  
+  // prints chessboard
+  console.log(hashes);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
--- projects/matchy/functions.js
+++ projects/matchy/functions.js
@@ -13,25 +13,91 @@
 // Step 1 - Search ///////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
+// function called search that accepts two parameters
+function search(animals, string) {
+    // for loop that loops through the 1st input of the funtion
+    // start: 0
+    // stop: # of elements in arr input
+    for(var i = 0; i < animals.length; i++) {
+        // if statement that checks if the value of the name key of the ith element
+        // of the animals array is equal to the string input
+        if (animals[i]['name'] == string) {
+            // if true, returns the ith value of the animals array
+            return(animals[i]);
+        }
+    }
+    // if no matches were found, returns null
+    return(null);
+}
 
 
 //////////////////////////////////////////////////////////////////////
 // Step 2 - Replace //////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
-
+// function called replace that accepts three parameters: the animals array,
+// the name of an animal, and a replacement object
+function replace(animals, name, replacement) {
+    // for loop that loops through the animals array
+    // start: 0
+    // stop: # of elements in animals array
+    for (var i = 0; i < animals.length; i++) {
+        // if statement that checks if the value of name key of the ith element 
+        // of the animals array is equal to the name input
+        if (animals[i]['name'] == name) {
+            // if true, replaces the ith element of the animals array with the 
+            // object input
+            animals[i] = replacement;
+        }
+    }
+}
 
 //////////////////////////////////////////////////////////////////////
 // Step 3 - Remove ///////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
-
+// function named remove that accepts two inputs: the animals array
+// and a name
+function remove(animals, name) {
+    // for loop that loops through the animals array
+    // start: 0
+    // stop: # of elements in animals array
+    for (var i = 0; i < animals.length; i++) {
+        // if statement that checks if the value of name key of the ith element 
+        // of the animals array is equal to the name input
+        if (animals[i]['name'] === name) {
+            // if true, removes the ith element of the animals array
+            animals[i] = '';
+        }
+    }
+}
 
 //////////////////////////////////////////////////////////////////////
 // Step 4 - Add ///////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
-
+// function called add that accepts two inputs: the animals array and an animal 
+// object
+function add(animals, animal) {
+    // creates an empty list array
+    var list = [];
+    // for loop that loops through the animals array
+    // start: 0
+    // stop: # of elements in the animals array
+    for(var i = 0; i < animals.length; i++) {
+        // adds each animals name to the name array
+        list.push(animals[i]['name']);
+    }
+    // check that animal has a name property that is not empty, that each animal
+    // has a species property that is not empty, and that the name of the animal
+    // passed in through the animal input is not included in the list array that
+    // contains all animals names
+    if ((animal.name != '') && (animal.species != '') && (list.includes(animal['name']) === false)) {
+        // if true, push the animal object from the animal input onto the end of
+        // the animals array
+        animals.push(animal);
+    }
+}
 
 /**
  * You did it! You're all done with Matchy!
